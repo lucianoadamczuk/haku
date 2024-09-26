da58026a682e8840a90b881d88e953157b9b55dc
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-09-25 22:59:03 -0300

    feat(configuration): :wrench: add metadata, static props and function to handle unmatched languages

--- src/app/i18n/types/i18next.d.ts
+++ src/app/i18n/types/i18next.d.ts
@@ -1,11 +1,13 @@
 import 'i18next';
 
 /* --------------------------------- global --------------------------------- */
+import metadata from '../locales/es/global/metadata.json';
 import routes from '../locales/es/global/routes.json';
 
 declare module 'i18next' {
 	interface CustomTypeOptions {
 		resources: {
+			'global/metadata': typeof metadata;
 			'global/routes': typeof routes;
 		};
 	}
---
+++ src/modules/types/StaticParams.ts
@@ -0,0 +1,3 @@
+export interface StaticParams {
+	language: string;
+}
